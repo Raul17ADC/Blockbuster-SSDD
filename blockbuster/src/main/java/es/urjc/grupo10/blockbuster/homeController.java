6a2c1f2e204124be168668452e563af395569937
Pradas01 <alexpradas01@gmail.com>
2023-03-29 16:21:00 +0200

    rt

---
+++ blockbuster/src/main/java/es/urjc/grupo10/blockbuster/homeController.java
@@ -0,0 +1,14 @@
+package es.urjc.grupo10.blockbuster;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class homeController {
+    @GetMapping("/home")
+    public String greeting(Model model) {
+        model.addAttribute("name", "Blockbuster");
+        return "home_template";
+    }
+}

67b8ef5ea25bb217726dbfc59aaa4a7cedc5b31a
Raul17ADC <92937037+Raul17ADC@users.noreply.github.com>
2023-04-06 11:17:58 +0200

    Código formateado
    He formateado el código para que se vea "bonito"

--- blockbuster/src/main/java/es/urjc/grupo10/blockbuster/GreetingController.java
+++ blockbuster/src/main/java/es/urjc/grupo10/blockbuster/GreetingController.java
@@ -1,45 +1,47 @@
 package es.urjc.grupo10.blockbuster;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-    @Controller
-
+@Controller
 public class GreetingController {
     @GetMapping("/home")
-    public String greeting(Model model) {        
+    public String greeting(Model model) {
         return "home_template";
     }
+
     @GetMapping("/series")
-    public String greeting_series(Model model) {        
+    public String greeting_series(Model model) {
         return "series_template";
     }
+
     @GetMapping("/peliculas")
-    public String greeting_peliculas(Model model) {        
+    public String greeting_peliculas(Model model) {
         return "peliculas_template";
     }
+
     @GetMapping("/registro")
-    public String greeting_resgistro(Model model) {        
+    public String greeting_resgistro(Model model) {
         model.addAttribute("includeCSS", "registro.css"); // Agrega esta línea para incluir el archivo CSS
         model.addAttribute("includeJS", "registro.js");
         return "registro.html";
     }
 
     @GetMapping("/error")
-    public String error(Model model) {        
+    public String error(Model model) {
         return "error";
     }
 
     @GetMapping("/pelicula")
-    public String pelicula(Model model) {        
+    public String pelicula(Model model) {
         return "pelicula_template";
     }
+
     @GetMapping("/prueba")
-    public String prueba(Model model) {        
+    public String prueba(Model model) {
         return "prueba";
     }
 }
--- blockbuster/src/main/resources/static/json/registro.js
+++ blockbuster/src/main/resources/static/json/registro.js
@@ -1,28 +1,29 @@
-const form = document.querySelector('#register-form');
-const emailInput = document.querySelector('#email');
-const usernameInput = document.querySelector('#username');
-const passwordInput = document.querySelector('#password');
-const registerButton = document.querySelector('#register-button');
+const form = document.querySelector("#register-form");
+const emailInput = document.querySelector("#email");
+const usernameInput = document.querySelector("#username");
+const passwordInput = document.querySelector("#password");
+const registerButton = document.querySelector("#register-button");
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    
-    const email = emailInput.value;
-    const username = usernameInput.value;
-    const password = passwordInput.value;
-    
-    // TODO: Enviar datos del formulario al servidor para registrar el usuario
-    
-    console.log(`Email: ${email}`);
-    console.log(`Nombre de Usuario: ${username}`);
-    console.log(`Contraseña: ${password}`);
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const email = emailInput.value;
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  // TODO: Enviar datos del formulario al servidor para registrar el usuario
+
+  console.log(`Email: ${email}`);
+  console.log(`Nombre de Usuario: ${username}`);
+  console.log(`Contraseña: ${password}`);
 });
+
 const registerForm = document.getElementById("register-form");
 const confirmMessage = document.getElementById("confirm-message");
 const goHomeButton = document.getElementById("go-home");
 
 registerForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    confirmMessage.innerHTML = "Perfecto, ya te has registrado";
-    goHomeButton.style.display = "block";
+  event.preventDefault();
+  confirmMessage.innerHTML = "Perfecto, ya te has registrado";
+  goHomeButton.style.display = "block";
 });

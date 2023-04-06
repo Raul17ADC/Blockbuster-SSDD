97063044c1f776c4bd2f6d0d05a71f0d8965061a
manzar99 <99manzar@gmail.com>
2023-04-06 20:00:23 +0200

    pueba de menu desplegable
    madre mia

--- blockbuster/src/main/resources/static/json/home_login.js
+++ blockbuster/src/main/resources/static/json/home_login.js
@@ -1,29 +1,17 @@
-// Al cargar la página
-window.onload = function() {
+const dropdownBtn = document.querySelector('.dropdown-btn');
 
-    // Obtener la imagen de perfil
-    var profileIcon = document.querySelector(".profile-icon");
-  
-    // Obtener el menú desplegable
-    var profileMenu = document.querySelector(".profile-menu");
-  
-    // Al hacer clic en la imagen de perfil
-    profileIcon.onclick = function() {
-      profileMenu.classList.toggle("show");
-    }
-  
-    // Al hacer clic en cerrar sesión
-    var logoutButton = document.querySelector("#logout-button");
-    logoutButton.onclick = function() {
-      alert("Sesión cerrada");
-    }
-  
-    // Si el usuario hace clic fuera del menú desplegable, se cierra
-    window.onclick = function(event) {
-      if (!event.target.matches('.profile-icon')) {
-        if (profileMenu.classList.contains('show')) {
-          profileMenu.classList.remove('show');
-        }
-      }
-    }
-  }
+dropdownBtn.addEventListener('click', function() {
+  this.nextElementSibling.classList.toggle('show');
+});
+
+window.addEventListener('click', function(event) {
+  if (!event.target.matches('.dropdown-btn')) {
+    const dropdowns = document.getElementsByClassName('dropdown-content');
+    for (let i = 0; i < dropdowns.length; i++) {
+      const dropdown = dropdowns[i];
+      if (dropdown.classList.contains('show')) {
+        dropdown.classList.remove('show');
+     }
+}
+}
+});
